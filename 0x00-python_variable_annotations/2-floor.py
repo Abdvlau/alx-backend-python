1df82d8045b0b79dee31ff178bb4e9bc0493b1b0
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-07-04 16:40:14 +0100

    A function that take n as argument and returns its floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
-import math
 """ A module that contains a single function that returns the floor of it's argument"""
+import math
 
 
 def floor(n: float):
-    """Funtion that returns the floor of n"""
+    """returns the floor of a float"""
     return math.floor(n)
