54859f091bab6fbb08832b9d33a06c62ef458c17
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-07-04 16:23:13 +0100

    Made changes to match pycodestyl and mypy

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """A module that contains a single function that concatinate two arguments"""
 
+
 def concat(str1: str, str2: str) -> str:
     """A function that concatinate two strings"""
     return str1 + str2
