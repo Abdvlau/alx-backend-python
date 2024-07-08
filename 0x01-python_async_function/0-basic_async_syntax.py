8ba591ce1c2b96190ba4abc8a66e78dad4a5f374
Abdvlau <abdulabdullahiumar69@gmail.com>
2024-07-08 19:48:36 +0100

    An asynchronous function that takes an argument waits for a random delay and then eventually retuns it

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""This module contains an asynchronous """
+""" The basics of async  """
 import random
+import asyncio
 
 
-async def wait_random(max_delay=10):
-    """This function take an argument
-    that waits for a random delay and
-    eventually return it
-    """
-    delay = random.uniform(0, max_delay)
-
-    return delay
+async def wait_random(max_delay: int = 10) -> float:
+    """ asynchronous coroutine that takes in an integer argument
+        (max_delay, with a default value of 10) named wait_random that waits
+        for a random delay between 0 and max_delay (included and float value)
+        seconds and eventually returns it.  """
+    random_float = random.uniform(0, max_delay)
+    await asyncio.sleep(random_float)
+    return random_float
